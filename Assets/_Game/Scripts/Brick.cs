88d8fcc8c527cdb7d0c59c8531985cbf36484471
NguyenVanTuyen2002 <nguyenvantuyentsdemon@gmail.com>
2024-05-24 19:28:42 +0700

    ~+ Push game

---
+++ Assets/_Game/Scripts/Brick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public GameObject brickPrefab; // Đối tượng prefab
+    public int rows = 10; // Số hàng
+    public int columns = 10; // Số cột
+    public float spacing = 2.0f; // Khoảng cách giữa các đối tượng
+    public Vector3 startPosition; // Vị trí bắt đầu của lưới
+
+    void Start()
+    {
+        SpawnGrid();
+    }
+
+    void SpawnGrid()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                // Tính toán vị trí
+                Vector3 position = startPosition + new Vector3(i * spacing, 0, j * spacing);
+                // Tạo đối tượng
+                Instantiate(brickPrefab, position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Joystick joystick;
+    [SerializeField] private float speed = 5;
+
+    private bool onStairs = false;
+
+    void Update()
+    {
+        if (onStairs)
+        {
+            Move();
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        float v = joystick.Vertical;
+        float h = joystick.Horizontal;
+        Vector3 translate = (new Vector3(-h, 0, -v) * Time.deltaTime) * speed;
+        transform.Translate(translate);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra va chạm với cầu thang
+        if (other.CompareTag("Stairs"))
+        {
+            onStairs = true;
+            Debug.Log("hit");
+        }
+        else if (other.CompareTag("Brick"))
+        {
+            Debug.Log("box");
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Kiểm tra khi rời khỏi cầu thang
+        if (other.CompareTag("Stairs"))
+        {
+            onStairs = false;
+        }
+    }
+}
